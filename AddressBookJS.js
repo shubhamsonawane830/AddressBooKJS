e46ee01369c2eeb88c2d45f30800c4ab9c814ffa
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-09 08:03:25 +0530

    Added method searchByCityOrState to get contacts

--- AddressBookJS.js
+++ AddressBookJS.js
@@ -144,11 +144,22 @@ let deletContact=()=>{
     }
 }
 
+function searchByCityOrState(place, choice){
+    let contacts = new Array();
+    if(choice == 1){
+        contacts = contactArray.filter(contact => contact.city === place)
+    }
+    if(choice == 2){
+        contacts = contactArray.filter(contact => contact.state === place)
+    }
+    console.log("Contact: ",contacts);
+}
+
 
 let choice = 0;
 do{
     console.log("Press: \n1) Add Contact \n2) Edit Contact \n3) View Contact \n4) Delete Contact \n5) Find Number of Contacts in AddressBook");
-    console.log("0) Exit");
+    console.log("6) Search By City or State \n0) Exit");
     choice = Number(prompt("Enter your choice: "));
     if(choice == 1){
         addContact();
@@ -169,6 +180,18 @@ do{
     }
     if(choice == 5){
         console.log("Number of Contacts: "+contactArray.reduce(contacts => contacts+1,0));
+    }
+    if(choice == 6){
+        console.log("1) Search By City     2) Search By State");
+        let ch = Number(prompt("Enter your choice: "));
+        switch (ch){
+            case 1: let city = prompt("Enter the city name: ");
+                    searchByCityOrState(city, 1);
+                    break;
+            case 2: let state = prompt("Enter the state name: ");
+                    searchByCityOrState(state, 2);
+                    break;
+        }
     }
 }while(choice != 0);
 
